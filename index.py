a77ec7a6860e367bcc3e82773e3b6e1d394349cf
Abdullah Al Faysal <faysal.iit0313@gmail.com>
2020-03-26 05:32:40 -0500

    json dump of daily data as backup done

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 from data_access_layer import save_videos
+from json_export import export_as_json
 from yt_trending_videos import get_daily_trending_videos, get_trending_videos
 
 if __name__ == "__main__":
-    # result = get_trending_videos("https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyDNTGIwf-TsAweg6-yFHWR4ZTkFPYlaeVE")
+    # results = get_trending_videos("https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyDNTGIwf-TsAweg6-yFHWR4ZTkFPYlaeVE")
     results = get_daily_trending_videos()
     print(len(results))
     save_videos(results)
+    export_as_json(results)
---
+++ json_export.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+import json
+import logging
+import os.path
+
+logging.basicConfig(filename='yttracker.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger = logging.getLogger(__name__)
+
+
+def export_as_json(data):
+    try:
+        file_Base_location = 'C:\\Users\\aafaysal\\[ MEGA ]\\UALR\\Spring 2019\\Thesis\\daily_json_dumps\\'
+        daily_file_name = datetime.now().strftime("%Y-%m-%d-%H-%M") + ".json"
+        compiled_file_name = "compiled.json"
+        daily_file_path = os.path.join(file_Base_location + daily_file_name)
+        compiled_file_path = os.path.join(file_Base_location + compiled_file_name)
+        with open(daily_file_path, 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+
+        with open(compiled_file_path, 'a+') as json_file:
+            if os.path.getsize(compiled_file_path) == 0:
+                json_file.seek(0)
+                json.dump([], json_file, indent=4)
+            json_file.seek(0)
+            prev_data = json.load(json_file)
+            data = prev_data + data
+            json_file.seek(0)
+            json_file.truncate()
+            json.dump(data, json_file, indent=4)
+        return None
+    except Exception as err:
+        print("json export error >> ", err)
+        logger.error(err)
+        return None
